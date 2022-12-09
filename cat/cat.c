f67d9cd88406e10a286778803423168ffb48e0a1
kefirRzevo <golubovich.ts@phystech.edu>
2022-12-10 01:11:42 +0300

    cat fixed

--- cat/cat.c
+++ cat/cat.c
@@ -4,56 +4,41 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <assert.h>
 
-const int BUF_MAX_SIZE = 4096;
-
-
-int my_cat(int input, int output);
-int my_write(int fd, const void* buf, size_t nbyte);
-
-int my_write(int fd, const void* buf, size_t nbyte)
+int cat(int in, int out)
 {
-    int swb = 0;
-    while(swb < nbyte)
-    {
-        int wb = write(fd, buf + swb, nbyte - swb);
-        if(wb < 0)
-            return wb;
-        swb+=wb;
-    }
-    return swb;
-}
-
-
-int my_cat(int input, int output)
-{
-    char buf[BUF_MAX_SIZE] = {};
+    char buf[32] = {};
     int n = 0;
-    int err = 0;
 
-    while((n = read(input, buf, BUF_MAX_SIZE)) > 0)
-        if((err = my_write(output, buf, n)) < 0)
-            fprintf(stderr, "ERROR %d: output %d can't be written.", err, output);
+    do
+    {
+        n = read(in, buf, sizeof(buf));
+        if(n < 0)
+            return n;
+            
+        n = write(out, buf, n);
+        if(n < 0)
+            return n;
+    }
+    while(n > 0);  
 
-    if(n < 0)
-        fprintf(stderr, "ERROR %d: input %d can't be read.", n, input);
-
-    return 1;
+    return 0;  
 }
-
 
 int main(int argc, char* argv[])
 {
     if(argc == 1)
-        my_cat(0, 1);
+        cat(0, 1);
 
     for(int i = 1; i < argc; i++)
     {
         int fd = open(argv[i], O_RDONLY);
         if(fd < 0)
-            fprintf(stderr, "FILE %s can't be opened.", argv[i]);
+            assert(0);
 
-        my_cat(fd, 1);
+        if(cat(fd, 1) < 0)
+            assert(0);
     }
 
     return 0;
