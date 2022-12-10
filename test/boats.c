aba57c33f8fcd9851e71098bd1377e17e3cc2e9a
kefirRzevo <golubovich.ts@phystech.edu>
2022-12-10 19:18:31 +0300

    completed task 12

--- test/boats.c
+++ test/boats.c
@@ -8,8 +8,6 @@
 #include <sys/sem.h>
 #include <sys/shm.h>
 #include <time.h>
-#include <signal.h>
-
 
 //#define __DEBUG__
 
@@ -26,9 +24,10 @@ const useconds_t delay = 1000;
 
 enum sems
 {
-    trap   = 0,
+    trap = 0,
     sem_access = 1,
-    sem_exit   = 2,
+    sem_exit = 2,
+    sem_kill = 3,
 };
 
 
@@ -38,19 +37,18 @@ int is_error(const char* msg)
     return -1;
 }
 
-int boat(int n_trips, int boat_capacity)
+int boat(int n_trips, int boat_capacity, int n_passangers)
 {
     struct sembuf V_access = {sem_access, 1, 0};
     struct sembuf W_access = {sem_access, 0, 0};
     struct sembuf V_exit = {sem_exit, 1, 0};
     struct sembuf W_exit = {sem_exit, 0, 0};
     struct sembuf W_num2_trap[2] = {{trap, -2, 0}, {trap, 2, 0}};
-
+    
     for(size_t i = 0; i < n_trips; i++)
     {
         dump("Новый заход\n");
 
-        int n_pas = 0;
         for(int n_pas = 0; n_pas < boat_capacity; n_pas++)
         {
             if(semop(semid, &V_access, 1) == -1)
@@ -74,7 +72,18 @@ int boat(int n_trips, int boat_capacity)
                 return is_error("Bad semop W_exit\n");
         }
     }
-$$
+
+    struct sembuf V_kill = {sem_kill, 1, 0};
+    struct sembuf V_access_V_trap[2] = {{sem_access, 1, 0}, {trap, 1, 0}};
+
+    for(int n_pas = 0; n_pas < n_passangers; n_pas++)
+    {
+        if(semop(semid, &V_kill, 1) == -1)
+            return is_error("Bad semop V_kill\n");
+        if(semop(semid, V_access_V_trap, 2) == -1)
+            return is_error("Bad semop V_access\n");
+    }
+
     return 0;
 }
 
@@ -83,11 +92,15 @@ int passenger(int n_passenger)
     struct sembuf P_access_P_trap[2] = {{sem_access, -1, 0}, {trap, -1, 0}};
     struct sembuf P_exit_P_trap[2] = {{sem_exit, -1, 0}, {trap, -1, 0}};
     struct sembuf V_trap = {trap, 1, 0};
+    struct sembuf P_kill_no_wait = {sem_kill, -1, IPC_NOWAIT};
 
     while(1)
     {
         if(semop(semid, P_access_P_trap, 2) == -1)
             return is_error("Bad semop P_access_P_trap\n");
+
+        if(semop(semid, &P_kill_no_wait, 1) != -1)
+            return 0;
 
         dump("Пассажир %d отправился по трапу\n", n_passenger);
         usleep(delay*(rand()%100));
@@ -118,8 +131,8 @@ int main(int argc, char *argv[])
     int n_passangers = atoi(argv[2]);
     int boat_capacity = atoi(argv[3]);
 
-    //create three semaphores
-    semid = semget(IPC_PRIVATE, 3, 0666 | IPC_CREAT);
+    //create four semaphores
+    semid = semget(IPC_PRIVATE, 4, 0666 | IPC_CREAT);
     if(semid == -1)
         return is_error("Bad semid\n");
 
@@ -138,19 +151,19 @@ int main(int argc, char *argv[])
     if(semctl(semid, sem_exit, SETVAL, arg) == -1)
         return is_error("Bad SETVAL for sem exit\n");
 
+    arg.val = 0;
+    if(semctl(semid, sem_kill, SETVAL, arg) == -1)
+        return is_error("Bad SETVAL for sem kill\n");
+
     pid_t boat_pid = fork();
     if(boat_pid == 0)
     {
-        return boat(n_trips, boat_capacity);
+        return boat(n_trips, boat_capacity, n_passangers);
     }
     else if(boat_pid == -1)
     {
         return is_error("Bad fork\n");
     }
-
-    pid_t* childs = (pid_t* )malloc(sizeof(pid_t)*n_passangers);
-    if(!childs)
-        return is_error("Bad malloc\n");
 
     //run passengers processes
     for(size_t i = 0; i < n_passangers; i++)
@@ -162,19 +175,17 @@ int main(int argc, char *argv[])
         }
         else if(passanger_pid == -1)
         {
-            return free(childs), is_error("Bad fork\n");
+            return is_error("Bad fork\n");
         }
-        childs[i] = passanger_pid;
     }
 
     if(waitpid(boat_pid, NULL, 0) == -1)
-        return free(childs), is_error("Bad waitpid\n");
+        return is_error("Bad waitpid\n");
 
     for(size_t i = 0; i < n_passangers; i++)
-        if(kill(childs[i], SIGTERM) == -1)
-            return free(childs), is_error("Bad kill\n");
+        if(wait(NULL) == -1)
+            return is_error("Bad wait\n");
 
-    free(childs);
     if(semctl(semid, 3, IPC_RMID) == -1)
         return is_error("Bad IPC_RMID for semaphores\n");
 
